fa3fc71807fbce2c440d119be5b914c4ec228ccc
kelvinng2016 <kelvinngshuhui2016@gmail.com>
2017-07-18 16:40:47 +0800

    change

--- app/src/main/java/kelvinng2017/pushup/Main2Activity.java
+++ app/src/main/java/kelvinng2017/pushup/Main2Activity.java
@@ -3,17 +3,11 @@ package kelvinng2017.pushup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    private void  TestUpload(){
-        int i=1+1;
-        int ii=2+2;
-        int iii=i+ii;
+        setContentView(R.layout.activity_main2);
     }
 }
